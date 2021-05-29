4898e2d0c04b2c487f77c144865dab04542d79ee
Time Cipher <70690930+TimeCipher@users.noreply.github.com>
2021-05-29 01:31:17 +0100

    Update client.py

--- client.py
+++ client.py
@@ -3,32 +3,51 @@ import websockets
 import time
 import json
 import base64
+from threading import Thread
 
-AuthList = ['auth']
-Auth = input('Password: ')
-Auth64 = base64.b64encode(Auth.encode())
-AuthList.append(Auth64.decode())
-
-JsonAuth = json.dumps(AuthList)
-
+MultiThread = False
 Mining = True
 
-async def mine():
+if MultiThread == True:
+    AmountOfPasswords = int(input('Number of threads: '))
+
+JsonAuthList = []
+
+def AddAuth():
+    Auth = input('Password: ')
+    Auth64 = base64.b64encode(Auth.encode()).decode()
+    JsonAuth = json.dumps(['auth', {'password': Auth64}])
+    return JsonAuth
+
+JsonAuthList.append(AddAuth())
+
+async def mine(Pos):
     global JsonAuth
     #uri = "ws://localhost:8765"
     uri = 'ws://188.165.82.203:90'
     async with websockets.connect(uri) as websocket:
 
-        await websocket.send(JsonAuth)
-        print(f"> {JsonAuth}")
+        await websocket.send(JsonAuthList[Pos])
+        print(f"> {JsonAuthList[Pos]}")
 
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
+        reply = json.loads(await websocket.recv())
+        print(f'< {reply}')
 
-'''
-while Mining == True:   
-    asyncio.get_event_loop().run_until_complete(mine())
-    time.sleep(5)
+        mineReq = json.dumps(['mine'])
+        await websocket.send(mineReq)
+        print(f"> {mineReq}")
 
-'''
-asyncio.get_event_loop().run_until_complete(mine())
+        reply = json.loads(await websocket.recv())
+        print(f'< {reply}')
+
+ListPos = 0
+while Mining == True: 
+    if MultiThread == True:
+        ListPos = ListPos + 1  
+        Thread(target = asyncio.get_event_loop().run_until_complete(mine(ListPos))).start()
+        if ListPos == AmountOfPasswords:
+            ListPos = 0
+            time.sleep(7)
+    else:
+        asyncio.get_event_loop().run_until_complete(mine(0))
+        time.sleep(7)
