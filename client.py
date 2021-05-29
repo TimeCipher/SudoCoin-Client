26d0a9355aa6a82ea82e98c4c52a7ab1f610ba35
Time Cipher <70690930+TimeCipher@users.noreply.github.com>
2021-05-29 10:44:17 +0100

    Update client.py

--- client.py
+++ client.py
@@ -5,6 +5,9 @@ import json
 import base64
 from threading import Thread
 
+uri = 'ws://188.165.82.203:90'
+SOCKET = websockets.connect(uri)
+
 MultiThread = False
 Mining = True
 
@@ -21,17 +24,16 @@ def AddAuth():
 
 JsonAuthList.append(AddAuth())
 
-async def mine(Pos):
-    global JsonAuth
-    #uri = "ws://localhost:8765"
-    uri = 'ws://188.165.82.203:90'
-    async with websockets.connect(uri) as websocket:
-
+async def AuthFunc(Pos):
+    async with SOCKET as websocket:
         await websocket.send(JsonAuthList[Pos])
         print(f"> {JsonAuthList[Pos]}")
 
         reply = json.loads(await websocket.recv())
         print(f'< {reply}')
+
+async def mine():
+    async with SOCKET as websocket:
 
         mineReq = json.dumps(['mine'])
         await websocket.send(mineReq)
@@ -40,14 +42,18 @@ async def mine(Pos):
         reply = json.loads(await websocket.recv())
         print(f'< {reply}')
 
+if MultiThread == False:
+    asyncio.get_event_loop().run_until_complete(AuthFunc(0))
+
 ListPos = 0
 while Mining == True: 
     if MultiThread == True:
+        Thread(target = asyncio.get_event_loop().run_until_complete(AuthFunc(ListPos))).start()
         ListPos = ListPos + 1  
-        Thread(target = asyncio.get_event_loop().run_until_complete(mine(ListPos))).start()
+        Thread(target = asyncio.get_event_loop().run_until_complete(mine())).start()
         if ListPos == AmountOfPasswords:
             ListPos = 0
             time.sleep(7)
     else:
-        asyncio.get_event_loop().run_until_complete(mine(0))
+        asyncio.get_event_loop().run_until_complete(mine())
         time.sleep(7)
